629992ab7153f5b4246769e0c5bf1ed7e63c57c3
pauljianbo <paul@greywolf.sg>
2025-02-18 15:01:16 +0800

    add testing for useToggle hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-
+import ToggleComponent from "@/components/1-useToggle/toggleComponent";
 export default function Home() {
   return (
     <div>
-      <h1 className="font-sans">Hello World</h1>
-      <h1 className="font-mono">Hello World</h1>
+      <ToggleComponent />
     </div>
   );
 }
--- components/1-useToggle/toggleComponent.tsx
+++ components/1-useToggle/toggleComponent.tsx
@@ -1,15 +1,21 @@
+"use client";
 import useToggle from "@/hooks/1-useToggle/useToggle";
 const ToggleComponent = () => {
   const [value, toggleValue] = useToggle(false);
   return (
-    <div>
-      <h1>Toggle Component</h1>
-      <h1>{value.toString()}</h1>
-      <button onClick={() => toggleValue()}>Toggle</button>
-      <button onClick={() => toggleValue(true)}>Set True</button>
-      <button onClick={() => toggleValue(false)}>Set False</button>
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <h1>Result : {value.toString()}</h1>
+      <button className="bg-blue-500 text-white p-2 rounded-md" onClick={() => toggleValue()}>
+        Toggle
+      </button>
+      <button className="bg-blue-500 text-white p-2 rounded-md" onClick={() => toggleValue(true)}>
+        Set True
+      </button>
+      <button className="bg-blue-500 text-white p-2 rounded-md" onClick={() => toggleValue(false)}>
+        Set False
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default ToggleComponent;
