ab33de6170ad1589915a28101046dfed86eb2cb1
pauljianbo <paul@greywolf.sg>
2025-02-19 16:39:01 +0800

    update

--- components/1-useToggle/toggleComponent.tsx
+++ components/1-useToggle/toggleComponent.tsx
@@ -5,13 +5,13 @@ const ToggleComponent = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen gap-4">
       <h1>Result : {value.toString()}</h1>
-      <button className="bg-blue-500 text-white p-2 rounded-md" onClick={() => toggleValue()}>
+      <button className="bg-blue-500 text-white p-2 rounded-md " onClick={() => toggleValue()}>
         Toggle
       </button>
-      <button className="bg-blue-500 text-white p-2 rounded-md" onClick={() => toggleValue(true)}>
+      <button className="bg-blue-500 text-white p-2 rounded-md " onClick={() => toggleValue(true)}>
         Set True
       </button>
-      <button className="bg-blue-500 text-white p-2 rounded-md" onClick={() => toggleValue(false)}>
+      <button className="bg-blue-500 text-white p-2 rounded-md " onClick={() => toggleValue(false)}>
         Set False
       </button>
     </div>
